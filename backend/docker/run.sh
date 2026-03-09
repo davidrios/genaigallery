7e01fc783fc1f7dad4951bc730438141c534a591
David Rios <david.rios.gomes@gmail.com>
2026-03-09 19:24:20 -0300

    Also copy lib on module change

--- backend/docker/run.sh
+++ backend/docker/run.sh
@@ -4,21 +4,37 @@ echo Compiling backend server...
 
 if test "$COPY_GO_LIB" == "1"; then
 	# copy files to local to be able to use LSPs
-	mkdir -p ../gomods
-	rsync -a /go/ ../gomods/go/
-	rsync -a /usr/local/go/ ../gomods/usr-local-go/
+	(while true; do
+	watchmedo \
+		auto-restart \
+		-d /workdir/backend \
+		--patterns="go.mod;go.sum" \
+		--recursive \
+		-D \
+		--kill-after 1 \
+		--interval 0.3 \
+		--no-restart-on-command-exit \
+		sh -- -c '
+			mkdir -p ../gomods
+			rsync -a /go/ ../gomods/go/
+			rsync -a /usr/local/go/ ../gomods/usr-local-go/
+			echo go lib copied
+			sleep 1
+		'
+	sleep 1
+	done) &
 fi
 
 while true; do
-    watchmedo \
-        auto-restart \
-        -d /workdir/backend \
-        --patterns="*.go" \
-        --recursive \
-        -D \
-        --kill-after 1 \
-        --interval 0.3 \
+	watchmedo \
+		auto-restart \
+		-d /workdir/backend \
+		--patterns="*.go" \
+		--recursive \
+		-D \
+		--kill-after 1 \
+		--interval 0.3 \
 		sh -- -c 'go run --tags "fts5" cmd/server/main.go || pkill -f "/root/.cache/go-build" && sleep 1'
-    sleep 1
+	sleep 1
 done
 
